65f57384e3d6d8fe030ec9593d7be89ec6354f22
Ranji Raj <64376506+ranjiGT@users.noreply.github.com>
2021-05-25 19:45:33 +0200

    Add files via upload

---
+++ Uploaded Programs/Linked List/SLL(Global).C
@@ -0,0 +1,323 @@
+/* Singly Linked List (Basic Operations) - Program to create a linked list abstract data type and perform various operations on it (Variable first declared globally) */
+
+#include  <stdio.h>
+#include  <stdlib.h>
+#define  NULL 0
+
+/* Assume that the data portion of each node consists of ONLY an integer.*/
+struct  node
+{
+  int  data ;
+  struct  node  *next ;
+} ;
+
+struct  node  *first=NULL ;
+
+/* first is a global variable and need not be passed to any function. Any changes made to variable first by any of the functions in the program will be permanent and will be reflected in the entire program */
+
+/* This function is responsible for creating the Linked List right from the BEGINING. */
+void  create()
+{
+  int  i , n ;
+  struct  node  *pnode , *p ;
+
+  printf("Enter the number of nodes required:\n") ;
+  scanf("%d",&n) ;
+
+  printf("Enter the data value of each node:\n") ;
+  for(i=1 ; i<=n ; i++)
+  {
+    pnode=(struct node*)malloc(sizeof(struct node)) ;
+
+    if(pnode==NULL)
+    {
+      printf("Memory overflow. Unable to create.\n") ;
+      return ;
+    }
+
+    scanf("%d",&pnode->data) ;
+
+    /* New node will be the last node */
+    pnode->next=NULL ;  
+
+    if(first==NULL)
+    	 first=p=pnode ;
+    else
+    {
+	    p->next=pnode ;
+	    p=pnode ;   /* p keeps track of last node */
+    }
+  }
+}
+
+
+/* This function will insert a new node with value x after a node with value K in an already existing Linked List */
+void  insertafter(int  x , int  k)
+{
+  struct  node  *pnode , *p ;
+  pnode=(struct node*)malloc(sizeof(struct node)) ;
+
+  if(pnode==NULL)
+  {
+    printf("Memory overflow. Unable to create.\n") ;
+    return ;
+  }
+
+  /* searching the required node */
+  p=first ;
+  while(p!=NULL)
+  {
+	if(p->data==k)
+	  break ;
+    p=p->next ;  }
+
+  /* p becomes NULL when required data is not found */
+  if(p==NULL)
+     printf("Required node not found.\n") ;
+  else
+  {
+    pnode->data=x ;
+
+    /* actual insertion */
+    pnode->next=p->next ;
+    p->next=pnode ;
+  }
+}
+
+/* This function will insert a new node with value x before a node with value K in an already existing Linked List */
+void  insertbefore(int  x , int  k)
+{
+  struct  node  *pnode , *p , *follow ;
+
+  pnode=(struct node*)malloc(sizeof(struct node)) ;
+  if(pnode==NULL)
+  {
+    printf("Memory overflow. Unable to create.\n") ;
+    return ;
+  }
+
+  /* searching the required node */
+  p=first ;
+  follow=NULL ;
+  while(p!=NULL)
+  {
+	if(p->data==k)
+	  break ;
+    follow=p ;
+    p=p->next ;
+  }
+
+  if(p==NULL)
+    printf("Required node not found.\n") ;
+  else
+  {
+    pnode->data=x ;
+
+    /* actual insertion */
+
+    if(p==first) /* inserting before the first node */
+    {
+      pnode->next=first ;
+      first=pnode ;
+    }
+    else          /* inserting before any other node */
+    {
+      follow->next=pnode ;
+      pnode->next=p ;
+    }
+  }
+}
+
+/* This function will delete a node with value k from the Linked List if such a node exists */
+void  deletenode(int  k)
+{
+  struct  node  *p , *follow ;
+
+  /* searching the node to be deleted */
+  p=first ;
+  follow=NULL ;
+  while(p!=NULL)
+  {
+	if(p->data==k)
+	  break ;  
+    follow=p ;
+    p=p->next ;
+  }
+
+  if(p==NULL)
+     printf("Required node not found.\n") ;
+  else
+  {
+    /* actual deletion */
+    if(p==first)   /* first node to be deleted */
+      first=first->next ;
+    else	   /* deleting any other node */
+      follow->next=p->next ;
+
+    free(p) ;  /* Memory De-allocation */
+  }
+}
+
+/* This function will search a node with value k in the Linked List */
+void  search(int  k)
+{
+  struct  node  *p ;
+
+  p=first ;
+  while(p!=NULL)
+  {
+	if(p->data==k)
+	  break ;
+  	p=p->next ;  }
+
+  if(p==NULL)
+     printf("Required node not found.\n") ;
+  else
+    printf("Required node is present at address %X \n",p) ;
+}
+
+/* This function go through all the nodes of Linked List exactly once and will display data value of each node */
+void  traverse()
+{
+  struct  node  *p ;
+  if(first==NULL)
+    printf("Linked List Empty") ;
+  else
+  {
+    printf("Linked List is as shown: \n") ;
+
+    p=first ;
+    while(p!=NULL)
+    {
+      printf("%d ",p->data) ;
+      p=p->next ;
+    }
+    printf("\n") ;
+  }
+}
+
+/* This function will count the number nodes in Linked List */
+void count()
+{
+  int  n=0 ;
+  struct  node  *p ;
+
+  p=first ;
+  while(p!=NULL)
+  {
+    n++ ;
+    p=p->next ;
+  }
+
+  printf("Number of nodes in Linked List is %d \n",n) ;
+}
+
+/* This function will reverse a given linked list without using additional memory allocation */
+void  reverse()
+{
+  struct  node  *p , *follow , *ahead ;
+
+  p=first ;
+  follow=ahead=NULL ;
+  while(p!=NULL)
+  {
+    ahead=p->next ;
+    p->next=follow ;
+    follow=p ;
+    p=ahead ;
+  }
+  first=follow ;
+}
+
+/* This function will make the Linked List empty. */
+void  destroylist()
+{
+  first=NULL ;
+}
+
+void  main()
+{
+  int  x , k , ch ;
+  clrscr() ;
+  do
+  {
+    printf("\n Menu: \n") ;
+    printf("1:Create Linked List \n") ;
+    printf("2:Insert After \n") ;
+    printf("3:Insert Before \n") ;
+    printf("4:Delete Node \n") ;
+    printf("5:Search Node \n") ;
+    printf("6:Traverse \n") ;
+    printf("7:Count \n") ;
+    printf("8:Reverse \n") ;
+    printf("9:Destroy List \n") ;
+    printf("10:Exit \n") ;
+
+    printf("\nEnter your choice: ") ;
+    scanf("%d",&ch) ;
+
+    switch(ch)
+    {
+      case 1:
+      create() ;
+      break ;
+
+      case 2:
+      printf("Enter the data value of new node: ") ;
+      scanf("%d",&x) ;
+      printf("Enter the data value of that node after which the new node is to be inserted: ") ;
+      scanf("%d",&k) ;
+      insertafter(x,k) ;
+      break ;
+
+      case 3:
+      printf("Enter the data value of new node: ") ;
+      scanf("%d",&x) ;
+      printf("Enter the data value of that node before which the new node is to be inserted: ") ;
+      scanf("%d",&k) ;
+      insertbefore(x,k) ;
+      break ;
+
+      case 4:
+      printf("Enter the data value of the node to be deleted: ") ;
+      scanf("%d",&k) ;
+      deletenode(k) ;
+      break ;
+
+      case 5:
+      printf("Enter the data value of the node to be searched: ") ;
+      scanf("%d",&k) ;
+      search(k) ;
+      break ;
+
+      case 6:
+      traverse() ;
+      break ;
+
+      case 7:
+      count() ;
+      break ;
+
+      case 8:
+      reverse() ;
+      break ;
+
+      case 9:
+      destroylist() ;
+      break ;
+
+      case 10:
+      break ;
+    }
+  }
+  while(ch!=10) ;
+
+  getch() ;
+}
+
+
+
+
+
+
+
---
+++ Uploaded Programs/Trees/BT.C
@@ -0,0 +1,230 @@
+/* Binary Tree (NOT Binary Search Tree) operations - Create , Copy , Mirror , Traverse , node count and leaf count */
+
+#include  <stdio.h>
+#include  <stdlib.h>
+#include  <conio.h>
+#define  NULL  0		
+
+/* Assume that the data portion of each node consists of an integer. */
+struct  node
+{
+  int  data ;
+  struct  node  *left , *right ;
+} ;
+
+int  ncount , lcount ;
+
+/* This function is responsible for creating a BT with multiple nodes */
+void  create(struct  node  **proot)
+{
+  int  x ;
+  
+  printf("Enter the data value of node.\n") ;
+  printf("Enter -1 for no data or NULL.\n") ;
+  scanf("%d",&x) ;
+
+  if(x==-1)
+    return ;
+
+  *proot=(struct node*)malloc(sizeof(struct node)) ;
+  (*proot)->data=x ;
+  (*proot)->left=(*proot)->right=NULL ;
+
+  printf("Enter the left child of %d.\n",x) ;
+  create(&((*proot)->left)) ;
+
+  printf("Enter the right child of %d.\n",x) ;
+  create(&((*proot)->right)) ;
+}
+
+/* This function will copy one binary tree into another */
+void  copy(struct  node  **proot1 , struct  node  *root)
+{
+  if(root==NULL)
+    return ;
+
+  *proot1=(struct node*)malloc(sizeof(struct node)) ;
+  (*proot1)->data=root->data ;
+  (*proot1)->left=(*proot1)->right=NULL ;
+
+  copy(&((*proot1)->left) , root->left) ;
+  copy(&((*proot1)->right) , root->right) ;
+}
+
+
+/* This function will create a mirror image of original binary tree */
+void  mirror(struct  node  **proot2 , struct  node  *root)
+{
+  if(root==NULL)
+    return ;
+
+  *proot2=(struct node*)malloc(sizeof(struct node)) ;
+  (*proot2)->data=root->data ;
+  (*proot2)->left=(*proot2)->right=NULL ;
+
+  mirror(&((*proot2)->left) , root->right) ;
+  mirror(&((*proot2)->right) , root->left) ;
+}
+
+/* Binary Tree Traversal methods */
+void  preorder(struct  node  *root)
+{
+  if(root!=NULL)
+  {
+    /* Visit the root */
+    printf("%d " , root->data) ;
+    /* Traverse the left subtree in preorder */
+    preorder(root->left) ;
+    /* Traverse the right subtree in preorder */
+    preorder(root->right) ;
+  }
+}
+
+void  inorder(struct  node  *root)
+{
+  if(root!=NULL)
+  {
+    inorder(root->left) ;
+    printf("%d ",root->data) ;
+    inorder(root->right) ;
+  }
+}
+
+void  postorder(struct  node  *root)
+{
+  if(root!=NULL)
+  {
+    postorder(root->left) ;
+    postorder(root->right) ;
+    printf("%d ",root->data) ;
+  }
+}
+
+/* This function will count the total no. of nodes in the BT */
+void  nodecount(struct  node  *root)
+{
+  if(root!=NULL)
+  {
+    nodecount(root->left) ;
+    ++ncount ;
+    nodecount(root->right) ;
+  }
+}
+
+/* This function will count the total no. of leaf nodes in the BT. Moreover, it will also display data value of leaf nodes. */
+void  leafnodecount(struct  node  *root)
+{
+  if(root!=NULL)
+  {
+    leafnodecount(root->left) ;
+    if(root->left==NULL && root->right==NULL)
+    {
+      ++lcount ;
+      printf("%d ",root->data) ;
+    }
+    leafnodecount(root->right) ;
+  }
+}
+
+
+void main()
+{
+  struct node *root = NULL ;
+  struct node *root1 = NULL ;
+  struct node *root2 = NULL ;
+
+  int  ch ;
+ 
+  clrscr() ;
+
+  do
+  {
+    printf("1.Create. \n") ;
+    printf("2.Copy Tree. \n") ;
+    printf("3.Mirror Tree. \n") ;
+    printf("4.Preorder. \n") ;
+    printf("5.Inorder. \n") ;
+    printf("6.Postorder. \n") ;
+    printf("7.Node Count. \n") ;
+    printf("8.Leaf Count. \n") ;
+    printf("9.Exit\n") ;
+
+    printf("Enter your choice: ") ;
+    scanf("%d",&ch) ;
+
+    switch(ch)
+    {
+      case 1:
+      create(&root) ;
+      break ;
+
+      case 2:
+      copy(&root1,root) ;
+      printf("Preorder traversal of copied tree is: \n") ;
+      preorder(root1) ;
+      printf("\n") ;
+      printf("Inorder traversal of copied tree is: \n") ;
+      inorder(root1) ;
+      printf("\n") ;
+      printf("Postorder traversal of copied tree is: \n") ;
+      postorder(root1) ;
+      printf("\n") ;
+      break ;
+
+      case 3:
+      mirror(&root2,root) ;
+      printf("Preorder traversal of mirrored tree is: \n") ;
+      preorder(root2) ;
+      printf("\n") ;
+      printf("Inorder traversal of mirrored tree is: \n") ;
+      inorder(root2) ;
+      printf("\n") ;
+      printf("Postorder traversal of mirrored tree is: \n") ;
+      postorder(root2) ;
+      printf("\n") ;
+      break ;
+
+      case 4:
+      preorder(root) ; 
+      printf("\n") ;
+      break ;
+
+      case 5:
+      inorder(root) ;
+      printf("\n") ;
+      break ;
+
+      case 6:
+      postorder(root) ;
+      printf("\n") ;
+      break ;
+
+      case 7:
+      ncount=0 ;
+      nodecount(root) ;
+      printf("No. of nodes present in the tree are %d \n",ncount) ;
+      break ;
+
+      case 8:
+      lcount=0 ;
+      leafnodecount(root) ;
+      printf("No. of Leaf nodes present in the tree are %d \n",lcount) ;
+      break ;
+
+      case 9:
+      break ;
+    }
+  }
+  while(ch!=9) ;
+
+  getch() ;
+}
+
+
+
+
+
+
+
+
+
